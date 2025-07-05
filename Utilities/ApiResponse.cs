570b0c219ae371c7a41ef84b221e4c3eb5615125
JuanPabloGHC <jpghc552@hotmail.com>
2025-07-04 23:15:15 -0600

    feat: ApiResponse class

---
+++ Utilities/ApiResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskTaskerAPI.Utilities
+{
+    public class ApiResponse<T>
+    {
+        public int Code { get; set; }
+
+        public string Message { get; set; }
+
+        public T Data { get; set; }
+    }
+}
